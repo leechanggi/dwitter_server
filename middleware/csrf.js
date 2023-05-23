6fbf6b2bb06ffc11d0bb3c9a99960b8a48fd2d6a
leechanggi <67743720+leechanggi@users.noreply.github.com>
2023-05-23 17:26:16 +0900

    add

--- middleware/csrf.js
+++ middleware/csrf.js
@@ -1,33 +1,37 @@
-import { config } from '../config.js';
-import bcrypt from 'bcrypt';
+import { config } from "../config.js";
+import bcrypt from "bcrypt";
 
 const CSRF_SECRET_KEY = config.csrf.secretKey;
 
 export const csrfCheck = (req, res, next) => {
-  if (req.method === 'GET' || req.method === 'OPTIONS' || req.method === 'HEAD') {
+  if (
+    req.method === "GET" ||
+    req.method === "OPTIONS" ||
+    req.method === "HEAD"
+  ) {
     return next();
   }
 
-  const csrfHeader = req.get('_csrf-token');
+  const csrfHeader = req.get("_csrf-token");
 
   if (!csrfHeader) {
-    console.warn('', req.headers.origin);
+    console.warn("", req.headers.origin);
     return res.status(403).json({
       message: `SECURE_CSRF : IS_NOT_EXISTS : ${csrfHeader}`,
     });
   }
 
   validateCsrfToken(csrfHeader) //
-    .then(value => {
+    .then((value) => {
       if (!value) {
-        console.warn('', req.headers.origin, csrfHeader);
+        console.warn("", req.headers.origin, csrfHeader);
         return res.status(403).json({
           message: `SECURE_CSRF : IS_NOT_VALID : ${csrfHeader}`,
         });
       }
       next();
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       return res.status(500).json({
         message: `SECURE_CSRF : IS_NOT_VALID : ${csrfHeader}`,
