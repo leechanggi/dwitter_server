2ab63a7e682bd16d7da9b5ce1201c5ca09f3ef06
Chaplin <67743720+leechanggi@users.noreply.github.com>
2023-06-07 18:27:44 +0900

    Typescript 변경

--- src/app.ts
+++ src/app.ts
@@ -2,27 +2,19 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
 import "express-async-errors";
-// import * as OpenApiValidator from "express-openapi-validator";
 import helmet from "helmet";
 import morgan from "morgan";
-// import swaggerUI from "swagger-ui-express";
-// import YAML from "yamljs";
 
 import { config } from "./config.js";
 import { initSocket } from "./connection/socket.js";
 import { sequelize } from "./db/database.js";
-// import { authHandler } from "./middleware/auth.js";
 import { csrfCheck } from "./middleware/csrf.js";
 import rateLimiter from "./middleware/rateLimiter.js";
-// import * as apis from "./controller/index.js";
 
 import tweetsRouter from "./router/tweets.js";
 import authRouter from "./router/auth.js";
 
-// const openApiDoc = YAML.load("./api/openapi.yaml");
 const app = express();
-
-// DEP
 const ports = config.port;
 const CORS_ORIGIN = config.cors.origin;
 
@@ -38,18 +30,11 @@ const options = {
 };
 
 const corsOptions = {
-  origin: [CORS_ORIGIN],
+  origin: CORS_ORIGIN,
   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
   optionsSuccessStatus: 200,
-  credentials: true, // Access-Control-Allow-Credentials
+  credentials: true,
 };
-
-// function modulePathResolver(_, route, apiDoc) {
-//   const pathKey = route.openApiRoute.substring(route.basePath.length);
-//   const operation = apiDoc.paths[pathKey][route.method.toLowerCase()];
-//   const methodName = operation.operationId;
-//   return apis[methodName];
-// }
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -63,23 +48,8 @@ app.use(morgan("tiny"));
 app.use(rateLimiter);
 app.use(csrfCheck);
 
-// app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(openApiDoc));
 app.use("/auth", authRouter);
 app.use("/tweets", tweetsRouter);
-// app.use(
-//   OpenApiValidator.middleware({
-//     apiSpec: "./api/openapi.yaml",
-//     validateResponses: true,
-//     operationHandlers: {
-//       resolver: modulePathResolver,
-//     },
-//     validateSecurity: {
-//       handlers: {
-//         jwt_auth: authHandler,
-//       },
-//     },
-//   })
-// );
 
 app.use((req, res, next) => {
   res.sendStatus(404);
